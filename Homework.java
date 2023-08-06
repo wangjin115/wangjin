2167f956532363f97b7c6b7bf4d75e18208b7769
wangjin115 <1150553544@qq.com>
2023-08-06 15:37:03 +0900

    test

---
+++ Homework.java
@@ -0,0 +1,5 @@
+public class Homework{
+	public static void main(String[] args) {
+		System.out.println("姓名\t性别\t籍贯\t住址");
+	}
+}
---
+++ Var02.java
@@ -0,0 +1,15 @@
+public class Var02{
+	
+		//记录人的信息
+		int age = 30;
+		double score = 88.9;
+		char gender = '男';
+		String name = "king";
+public static void main(String[] args) {
+		System.out.println("人的信息如下：");
+		System.out.println("name");
+		System.out.println("age");
+		System.out.println("score");
+		System.out.println("gender");
+	}
+}
