ea770c5c941dcdc3298a501d7c3b3cf095f5a224
wangjin115 <1150553544@qq.com>
2023-08-17 16:43:58 +0900

    for相关练习

---
+++ For02.java
@@ -0,0 +1,13 @@
+public class For02 {
+	public static void main(String[] args) {
+		
+		int n = 10;
+		for (int i = 0; i <= n; i++) {
+		
+		System.out.println(i + "+" + (n - i) + "=n");
+		
+		}
+		
+		}
+	
+	}
